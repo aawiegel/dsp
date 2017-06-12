07b2d560172bd1b21d07e885185903c2ce9191e1
Aaron W <aawiegel@gmail.com>
2017-06-12 16:36:07 -0700

    rough draft

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,22 @@
 # Hint:  use Google to find python function
 
+import datetime
+
+
+
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+.
+
+
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+date_start = datetime.datetime.strptime('12312013', '%m%d%Y')
+date_stop = datetime.datetime.strptime('05282015', '%m%d%Y')
+
+print(date_stop - date_start)
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_start = datetime.datetime.strptime('15-Jan-1994', '%d-%b-%Y')
+date_stop = datetime.datetime.strptime('14-Jul-2015', '%d-%b-%Y')
+
+print(date_stop - date_start)
