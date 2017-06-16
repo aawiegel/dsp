f23781d66f5dcd9c1ab6f1243374309f97f408d2
Aaron W <aawiegel@gmail.com>
2017-06-15 21:54:18 -0700

    write emails

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv', 'r') as reader:
+    reader = csv.reader(reader)
+    faculty = [record for record in reader]
+    
+for record in faculty[1:]:
+    email_list.append(record[3])
+
+with open('emails.csv', 'w', newline='') as email_file:
+    writer = csv.writer(email_file, delimiter=',')
+    for email in email_list:
+        writer.writerow([email])
